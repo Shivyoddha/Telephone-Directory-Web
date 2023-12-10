05ad7676fba7cb2e7b4154d4a0b0dba279e9a534
Megha Arya <meghaarya26@gmail.com>
2023-12-10 17:15:51 +0530

    api stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # db/seeds.rb
+# db/seeds.rb
+Faculty.create(
+  name: "Jane Smith",
+  phoneno: "555-555-5555",
+  email: "jane.smith@example.com",
+  telephone: "111-111-1111",
+  site_name: "Another University",
+  joining_date: Date.today
+)
+
+# Add more faculty records as needed
