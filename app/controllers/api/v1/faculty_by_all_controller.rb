773eae20b11a6fcce3ebccfd44f4b4e9303999cc
Megha Arya <meghaarya26@gmail.com>
2023-12-11 13:07:37 +0530

    api with multiple filters

--- app/controllers/api/v1/faculty_by_all_controller.rb
+++ app/controllers/api/v1/faculty_by_all_controller.rb
@@ -1,10 +1,11 @@
+
 class Api::V1::FacultyByAllController < ApplicationController
   def index
-    name_condition = "name LIKE ?" if params[:name].present?
-    designation_condition = "designation LIKE ?" if params[:designation].present?
-    department_condition = "department_id = ?" if params[:department_id].present?
+    name_condition = "name LIKE ?" if params[:name].present? && params[:name] != 'null'
+    designation_condition = "designation LIKE ?" if params[:designation].present? && params[:designation] != 'null'
+    department_condition = "department_id = ?" if params[:department_id].present? && params[:department_id] != 'null'
 
-    conditions = [name_condition, designation_condition, department_condition].compact.join(" AND ")
+    conditions = [name_condition, designation_condition, department_condition].compact.join(" OR ")
 
     if conditions.present?
       @faculty_by_all = Faculty.where(conditions, "#{params[:name]}%", "#{params[:designation]}%", params[:department_id])
