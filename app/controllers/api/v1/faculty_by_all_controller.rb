57985c6b6eb4f20ca1a791508dffdd9d513c36d1
Megha Arya <meghaarya26@gmail.com>
2023-12-11 15:11:40 +0530

    changed code for multiple filters

--- app/controllers/api/v1/faculty_by_all_controller.rb
+++ app/controllers/api/v1/faculty_by_all_controller.rb
@@ -1,14 +1,39 @@
-
 class Api::V1::FacultyByAllController < ApplicationController
   def index
-    name_condition = "name LIKE ?" if params[:name].present? && params[:name] != 'null'
-    designation_condition = "designation LIKE ?" if params[:designation].present? && params[:designation] != 'null'
-    department_condition = "department_id = ?" if params[:department_id].present? && params[:department_id] != 'null'
+    name_condition = "name LIKE ?" if params[:name].present? 
+    designation_condition = "designation LIKE ?" if params[:designation].present? 
+    department_condition = "department_id = ?" if params[:department_id].present? 
 
-    conditions = [name_condition, designation_condition, department_condition].compact.join(" OR ")
+    if name_condition.present? && designation_condition.present? && department_condition.present?
+      condition_all = [name_condition, designation_condition, department_condition].compact.join(" AND ")
+    elsif name_condition.present? && designation_condition.present?
+      condition_name_desig = [name_condition, designation_condition].compact.join(" AND ")
+    elsif name_condition.present? && department_condition.present?
+      condition_name_dep = [name_condition, department_condition].compact.join(" AND ")
+    elsif designation_condition.present? && department_condition.present?  
+      condition_dep_desig = [designation_condition, department_condition].compact.join(" AND ")
+    end 
 
-    if conditions.present?
-      @faculty_by_all = Faculty.where(conditions, "#{params[:name]}%", "#{params[:designation]}%", params[:department_id])
+    if condition_all.present?
+      @faculty_by_all = Faculty.where(condition_all, "#{params[:name]}%", "#{params[:designation]}%", params[:department_id])
+      render json: @faculty_by_all
+    elsif condition_name_desig.present?
+      @faculty_by_all = Faculty.where(condition_name_desig, "#{params[:name]}%", "#{params[:designation]}%")
+      render json: @faculty_by_all
+    elsif condition_name_dep.present?
+      @faculty_by_all = Faculty.where(condition_name_dep, "#{params[:name]}%", params[:department_id])
+      render json: @faculty_by_all
+    elsif condition_dep_desig.present?
+      @faculty_by_all = Faculty.where(condition_dep_desig, "#{params[:name]}%", "#{params[:designation]}%", params[:department_id])
+      render json: @faculty_by_all
+    elsif name_condition.present?
+      @faculty_by_all = Faculty.where(name_condition, "#{params[:name]}%")
+      render json: @faculty_by_all
+    elsif designation_condition.present?
+      @faculty_by_all = Faculty.where(designation_condition, "#{params[:name]}%")
+      render json: @faculty_by_all
+    elsif department_condition.present?
+      @faculty_by_all = Faculty.where(department_condition, "#{params[:name]}%")
       render json: @faculty_by_all
     else
       render_all_faculty
