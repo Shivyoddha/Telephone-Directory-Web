450fe93f682912cb99de6a474f6025b3d622a8b1
Megha Arya <153416455+mayghhaa@users.noreply.github.com>
2024-02-04 20:06:33 +0530

    Update faculties_controller.rb

--- app/controllers/faculties_controller.rb
+++ app/controllers/faculties_controller.rb
@@ -11,7 +11,7 @@ class FacultiesController < ApplicationController
           }
         ) or return
 
-        faculties = @filterrific.find.distinct.order(:custom_order, :designation, :joining_date)
+        faculties = @filterrific.find.distinct.order(:custom_order, :position, :designation, :joining_date)
         @faculties_by_department = faculties.group_by { |faculty| faculty.department.name }
 
         @departments = Department.order(:custom_order, :name)
