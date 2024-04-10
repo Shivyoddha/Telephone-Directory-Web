03ebd6757bd821a1513a373608a8dfcf0806be11
Anish Kumbhar <97729353+Shivyoddha@users.noreply.github.com>
2024-04-10 12:25:32 +0530

    Update departments_controller.rb

--- app/controllers/api/v1/departments_controller.rb
+++ app/controllers/api/v1/departments_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::DepartmentsController < ApplicationController
   def index
-    @departments = Department.all
+    @departments = Department.all.where.not(name:'Backup')
     render json: @departments
   end
 end
