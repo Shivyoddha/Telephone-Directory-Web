65b14e71c052676427dacd375b253a7d65e39c73
Anish Kumbhar <97729353+Shivyoddha@users.noreply.github.com>
2024-04-27 19:15:23 +0530

    Update faculties_controller.rb

--- app/controllers/api/v1/faculties_controller.rb
+++ app/controllers/api/v1/faculties_controller.rb
@@ -1,26 +1,32 @@
-
-# class Api::V1::FacultiesController < ApplicationController
-#   def index
-#     @faculties = Faculty.all
-
-#     render json: @faculties
-#   end
-# end
-
-# class Api::V1::FacultiesController < ApplicationController
-#   def index
-#     @faculties = Faculty.all
-#     render json: @faculties.as_json(only: [:id, :name, :email], methods: [:profile_image_url])
-#   end
-# end
-
-
-
 class Api::V1::FacultiesController < ApplicationController
   def index
-    @faculties = Faculty.includes(:department).where.not(departments: { name: 'Backup' })
-                     .order("departments.custom_order ASC,faculties.custom_order ASC, faculties.designation_id ASC, faculties.joining_date ASC, faculties.name ASC")
+    faculties = Faculty.includes(:department)
+                      .where.not(departments: { name: 'Backup' })
+                      .order("departments.custom_order ASC, faculties.custom_order ASC, faculties.designation_id ASC, faculties.joining_date ASC, faculties.name ASC")
 
-    render json: @faculties, methods: [:image_url]
+    # Group faculties by department
+    faculties_by_department = faculties.group_by { |faculty| faculty.department }
+
+    # Sort departments by custom order
+    sorted_departments = faculties_by_department.keys.sort_by { |department| department.custom_order }
+
+    # Construct sorted result array
+    sorted_faculties = sorted_departments.map do |department|
+      faculties_for_department = faculties_by_department[department]
+
+      # Sort faculties within department
+      sorted_faculties_for_department = faculties_for_department.sort_by do |faculty|
+        [
+          faculty.custom_order,
+          faculty.designation_id,
+          faculty.joining_date,
+          faculty.name
+        ]
+      end
+
+      { department: department, faculties: sorted_faculties_for_department }
+    end
+
+    render json: sorted_faculties, methods: [:image_url]
   end
 end
