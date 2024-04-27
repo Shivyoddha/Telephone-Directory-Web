2bb25b821307522c9b325b47dd786c19bf1565ed
Anish Kumbhar <97729353+Shivyoddha@users.noreply.github.com>
2024-04-27 21:46:49 +0530

    Update faculties_controller.rb

--- app/controllers/api/v1/faculties_controller.rb
+++ app/controllers/api/v1/faculties_controller.rb
@@ -1,7 +1,9 @@
-def index
-  @faculties = Faculty.includes(:department, :designation)
-                       .where.not(departments: { name: 'Backup' })
-                       .order("departments.custom_order ASC, departments.name ASC, faculties.custom_order ASC, designations.name ASC, faculties.joining_date ASC, faculties.name ASC")
+class Api::V1::FacultiesController < ApplicationController
+  def index
+    @faculties = Faculty.includes(:department, :designation)
+                         .where.not(departments: { name: 'Backup' })
+                         .order("departments.custom_order ASC, departments.name ASC, faculties.custom_order ASC, designations.name ASC, faculties.joining_date ASC, faculties.name ASC")
 
-  render json: @faculties, methods: [:image_url]
+    render json: @faculties, methods: [:image_url]
+  end
 end
