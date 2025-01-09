f8a6484630cee4945da14405f2d7bb78b5c2b61d
Shivyoddha2 <rekha.ani.74@gmail.com>
2025-01-09 11:07:47 +0530

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if @user.email=='bsnl@nitk.edu.in'
-      complaint_path
+      complaints_path
     else
       rails_admin_path
     end
